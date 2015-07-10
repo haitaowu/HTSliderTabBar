76539ced6b1117bd4f3d180ad8b3865731b8b3f2
haitaowu <taotaowoo@gmail.com>
2015-07-11 05:13:05 +0800

    Initial Commit

---
+++ HTSliderTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTSliderTabBar
+//
+//  Created by taotao on 15/7/11.
+//  Copyright (c) 2015年 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTSliderTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HTSliderTabBar
+//
+//  Created by taotao on 15/7/11.
+//  Copyright (c) 2015年 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
